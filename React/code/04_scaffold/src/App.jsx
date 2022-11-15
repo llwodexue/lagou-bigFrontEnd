a857333a6315d8a737b80245aaef765d190e140d
lilin <5575298+scx69@user.noreply.gitee.com>
2022-11-15 15:37:58 +0800

    scffold

---
+++ React/code/04_scaffold/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      message: 'Hello World'
+    }
+  }
+
+  render() {
+    const { message } = this.state
+    return (
+      <div>
+        <h2>{message}</h2>
+      </div>
+    )
+  }
+}
+
+export default App
