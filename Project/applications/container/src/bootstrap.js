195a845ec5e71d0dd2042dc31467ec51cfe5c893
lilin <5575298+scx69@user.noreply.gitee.com>
2023-05-23 09:18:14 +0800

    联邦模块

--- Project/applications/container/src/bootstrap.js
+++ Project/applications/container/src/bootstrap.js
@@ -1,13 +1,13 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./App"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
 function mount(el) {
   ReactDOM.render(<App />, el)
 }
 
-if (process.env.NODE_ENV === "development") {
-  const el = document.querySelector("#root")
+if (process.env.NODE_ENV === 'development') {
+  const el = document.querySelector('#root')
   if (el) mount(el)
 }
 
